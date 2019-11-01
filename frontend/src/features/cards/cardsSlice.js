ae1f3dd65eca0335b8d728893369ab42b4effba3
Joshua Cochran <joshuamcochran@gmail.com>
2019-11-01 16:06:50 -0700

    Learning redux

---
+++ frontend/src/features/cards/cardsSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from 'redux-starter-kit'
+
+let nextCardId = 0
+
+const cardsSlice = createSlice({
+    name: 'cards',
+    initialState: [],
+    reducers: {
+        addCard: {
+            reducer(state, action) {
+                const { id, text, description } = action.payload
+                state.push({ id, text, description })
+            },
+            prepare(text, description) {
+                return { payload: {text, description, id: nextCardId++ }}
+            }
+        },
+        deleteCard: {
+            reducer(state, action) {
+                state = state.filter(card => card.id !== action.payload.id)
+            }
+        }
+    }
+})
+
+export const { addCard, deleteCard } = cardsSlice.actions
+
+export default cardsSlice.reducer
---
+++ frontend/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "react-dom";
+import { configureStore } from "redux-starter-kit";
+import { Provider } from "react-redux";
+import "./css/index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import rootReducer from "./reducers";
+
+const store = configureStore({
+  reducer: rootReducer
+});
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
