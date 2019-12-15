61e8df9e34a13c44e94fef275b236eaed0c47478
Joshua Cochran <joshuamcochran@gmail.com>
2019-12-14 19:58:31 -0800

    Added additional selectors

--- frontend/src/BoardSelectors.js
+++ frontend/src/BoardSelectors.js
@@ -5,3 +5,13 @@ return Object.keys(state).find(key => object[key].isActive === true);
 export const getObjectById = (state, id) => { 
   return Object.keys(state).find(key => object[key].id === id);
 }
+
+export const getObjectIds = (state) => {
+  return state.map(item => item.id);
+}
+
+
+export const getTitle = (state) => { 
+  return state.title;
+}
+
